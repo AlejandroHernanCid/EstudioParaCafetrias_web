ff3d58e82eed0e43fd68cfd966d5390004ca238e
Alejandro Hernan Cid <hernanalejandro0017@gmail.com>
2025-08-05 12:30:30 +0200

    commit inicial web estudio cafeteria

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const track = document.querySelector(".carousel-track");
+  const slides = Array.from(track.children);
+  let currentIndex = 0;
+
+  function updateCarousel() {
+    const width = slides[0].getBoundingClientRect().width;
+    track.style.transform = `translateX(-${width * currentIndex}px)`;
+  }
+
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+  }, 3000); // 3 segundo
+
+  window.addEventListener("resize", updateCarousel);
+
+  // Carrusel de equipo
+  const equipoTrack = document.querySelector(".equipo-carousel-track");
+  const equipoSlides = Array.from(equipoTrack.children);
+  let equipoIndex = 0;
+
+  function updateEquipoCarousel() {
+    const width = equipoSlides[0].getBoundingClientRect().width;
+    equipoTrack.style.transform = `translateX(-${width * equipoIndex}px)`;
+  }
+
+  setInterval(() => {
+    equipoIndex = (equipoIndex + 1) % equipoSlides.length;
+    updateEquipoCarousel();
+  }, 3000); // cada 3 segundos
+
+  window.addEventListener("resize", updateEquipoCarousel);
+
+  const form = document.getElementById("contactForm");
+  form.addEventListener("submit", () => {
+    setTimeout(() => form.reset(), 100); // espera breve para que se envíe antes de limpiar
+  });
+});
